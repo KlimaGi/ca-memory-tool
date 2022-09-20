6bf0baa6449d503c5527d871a4213b67c5d2b1f0
Giedre <giedre.klima@gmail.com>
2022-09-20 11:32:28 +0300

    feat: add nav-bar elements

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import { Box } from '@mui/material';
 import NavBar from './components/nav-bar';
 
 const App: React.FC = () => (
-  <Box>
-    <h3>Boilerplate</h3>
-    <NavBar />
-  </Box>
+
+  <NavBar />
+
 );
 
 export default App;
--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -1,15 +1,37 @@
 import React from 'react';
-import { AppBar, Box } from '@mui/material';
-import logo from '../images/logo-white.png';
+import {
+  AppBar, Box, Typography, styled, Toolbar,
+} from '@mui/material';
+import { AddCircleRounded, AccountCircle } from '@mui/icons-material';
+import logo from '../images/logo-white-sm.png';
+
+const StyleToolBar = styled(Toolbar)({
+  display: 'flex',
+  justifyContent: 'space-between',
+});
 
 const NavBar = () => (
-  <AppBar>
-    <Box sx={{
-      padding: '1rem',
-    }}
-    >
-      <img src={logo} alt="logo" />
-    </Box>
+  <AppBar position="sticky">
+    <StyleToolBar>
+      <Box sx={{
+        padding: '0.5rem',
+        display: 'flex',
+        alignItems: 'center',
+      }}
+      >
+        <img src={logo} alt="logo" />
+        <Typography sx={{ display: { xs: 'none', sm: 'block' }, marginLeft: '1rem' }}>Memory tool</Typography>
+      </Box>
+
+      <Typography>About</Typography>
+      <Typography>Time line</Typography>
+
+      <AddCircleRounded />
+
+      <AccountCircle />
+
+    </StyleToolBar>
+
   </AppBar>
 );
 
